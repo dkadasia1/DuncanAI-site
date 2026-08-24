759f35505f33c999cbf1f50b6dfefb88c7fd5317
dkadasia1 <dkadasia@gmail.com>
2026-08-23 21:40:20 -0400

    Expose Supabase client globally

--- supabase-config.js
+++ supabase-config.js
@@ -8,7 +8,7 @@ const SUPABASE_URL =
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_3YaQFqnWC4FFbuMz6xmNuw_H5VDjBq2";
 
-const supabaseClient =
+window.supabaseClient =
   window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
