350d0933a4bdd0a465bf8b8bd1ebfafa77add2f1
dkadasia1 <dkadasia@gmail.com>
2026-08-23 21:04:06 -0400

    Add Supabase configuration

---
+++ supabase-config.js
@@ -0,0 +1,15 @@
+// =========================================
+// DuncanAI Supabase Configuration
+// =========================================
+
+const SUPABASE_URL =
+  "https://ipqljhgaoyqifgrhehf.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_3YaQFqnWC4FFbuMz6xmNuw_H5VDjBq2";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+  );
