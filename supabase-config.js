ad52ac6e4ac844d363d9f1c2695c52cb8c05c302
dkadasia1 <dkadasia@gmail.com>
2026-08-24 08:32:56 -0400

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -3,12 +3,12 @@
 // =========================================
 
 const SUPABASE_URL =
-  "https://ipqljhgaoyqifgrhehf.supabase.co";
+  "https://lpiqljhgaoyqifgrhehf.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_3YaQFqnWC4FFbuMz6xmNuw_H5VDjBq2";
 
-window.supabaseClient =
+const supabaseClient =
   window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
